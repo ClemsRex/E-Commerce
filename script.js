f4104dadc8fb28fd0e304736323d081311342262
ClemsRex <clementchukwu351@gmail.com>
2024-07-29 14:16:17 +0100

    Added the Shop page and the single product site, also started the blog page.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// hamburger functionality
+
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -13,3 +15,27 @@ if (close) {
         nav.classList.remove('active'); 
     })
 }
+
+        
+// single product funtionality
+
+var mainImg = document.getElementById("mainImg")
+
+var smallimg = document.getElementsByClassName("small-img")
+
+smallimg[0].onclick = function(){
+    mainImg.src = smallimg[0].src;
+}
+
+smallimg[1].onclick = function(){
+    mainImg.src = smallimg[1].src;
+}
+
+smallimg[2].onclick = function(){
+    mainImg.src = smallimg[2].src;
+}
+
+smallimg[3].onclick = function(){
+    mainImg.src = smallimg[3].src;
+}
+ 
